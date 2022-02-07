bd9e181da38cf925ddc00a6164bb24a9f43bf09e
alex <mifi123456798@gmail.com>
2022-02-07 17:52:40 +0300

    collection

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ansible Collection - my_own_namespace.yandex_cloud_elk
+
+Коллекция содержит роль writer, которая записывает текст в файл
+vars:
+	path - путь до файла
+	content - текст, записываемый в файл
+
+default values:
+	path: "test.txt"
+	content: "test content"
---
+++ plugins/modules/my_own_module.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    module_args = dict(
+        path=dict(type='str', required=True),
+        content=dict(type='str', required=True)
+    )
+
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    with open(module.params['path'], "w") as file:
+        file.write(module.params['content'])
+
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
